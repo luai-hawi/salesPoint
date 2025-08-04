9267cbebc69e39a82e2daa2e3414f87048e5b44c
unknown <luaihawi@gmail.com>
2025-08-04 20:36:55 +0300

    unsaved file

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -34,13 +34,13 @@
                         <input type="text" id="barcode_input" placeholder="Enter barcode..." class="w-1/3 px-3 py-2 border rounded">
                     </div>
                     <select name="customer_id" class="border rounded px-2 py-1 w-full mb-4">
-    <option value="">-- Select Customer (optional) --</option>
-    @foreach($customers as $customer)
-        <option value="{{ $customer->id }}" {{ old('customer_id') == $customer->id ? 'selected' : '' }}>
-            {{ $customer->name }} - {{ $customer->phone }}
-        </option>
-    @endforeach
-</select>
+                        <option value="">-- Select Customer (optional) --</option>
+                        @foreach($customers as $customer)
+                            <option value="{{ $customer->id }}" {{ old('customer_id') == $customer->id ? 'selected' : '' }}>
+                                {{ $customer->name }} - {{ $customer->phone }}
+                            </option>
+                        @endforeach
+                    </select>
 
                     <div id="products-list"></div>
 
